19eb8eabca39fa9cf4fb8863852c6ab92983af4a
April Wang from ETHZ <april.wang@inf.ethz.ch>
2024-06-30 17:16:51 +0200

    fix a folder directory address

--- server/kaggle_post_retrieve_module.py
+++ server/kaggle_post_retrieve_module.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.abspath(os.path.join(current_dir, '../../middle_file3.csv'))
+csv_path = os.path.abspath(os.path.join(current_dir, '../data/middle_file3.csv'))
 # Load the middle file
 # Define the function to get the URL by KernelId
 def get_kernel_url(kernel_version_id0):
