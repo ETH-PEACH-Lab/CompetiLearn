c3058e0a46dabcd065da193c86eb8ccf087c677f
Junling Wang <junlingwang@staff-net-ox-dock-1-a-090.intern.ethz.ch>
2024-07-24 21:30:13 +0200

    add feature of searchbar to adjust height based on input length

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,14 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
 import { Tooltip } from 'react-tooltip';
 
 const SearchBar = ({ onSearch }) => {
     const [query, setQuery] = useState('');
+    const textareaRef = useRef(null);
+    const maxRows = 14; // Maximum number of rows
+    const lineHeight = 24; // Adjust based on your CSS line height
 
     const handleInputChange = (e) => {
         setQuery(e.target.value);
+        autoResizeTextarea();
     };
 
     const handleSearchClick = () => {
@@ -20,18 +23,33 @@ const SearchBar = ({ onSearch }) => {
             e.preventDefault();  // Prevent default Enter key behavior
             handleSearchClick();
             setQuery('');
+            autoResizeTextarea();
         }
     };
+
+    const autoResizeTextarea = () => {
+        const textarea = textareaRef.current;
+        textarea.style.height = 'auto';
+        const newHeight = Math.min(textarea.scrollHeight, maxRows * lineHeight);
+        textarea.style.height = `${newHeight}px`;
+    };
+
+    useEffect(() => {
+        autoResizeTextarea(); // Initial resize on mount
+    }, []);
 
     return (
         <div className="search-bar">
             <textarea
                 id="search-textarea"
+                ref={textareaRef}
                 value={query}
                 onChange={handleInputChange}
+                onInput={autoResizeTextarea}  // Add onInput event
                 onKeyDown={handleKeyDown}  // Add the keydown event listener
                 placeholder="Enter your query..."
                 rows="1" // You can adjust the number of rows to fit your needs
+                style={{ overflow: 'hidden', resize: 'none' }} // Ensure the textarea looks good when resized
             />
             <button onClick={handleSearchClick} className='primary-button'> 
                     <Tooltip id="my-tooltip-search" />
